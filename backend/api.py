4d93cfb1bfc1f34c415d155e738fd82a3821f818
Amir Heyward <amirheyward2@gmail.com>
2026-07-23 15:41:06 -0400

    added backed using yt-dlp

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
+import yt_dlp
 
-print("hi")
+ydl_opts = {
+    "no_warnings": True,
+    "no_playlist": True
+}
+
+ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+app = FastAPI()
+
+@app.get("/find")
+def find_song_url(q: str):
+    results = ydl.extract_info(f"ytsearch1:{q}", download=False)
+    video = results["entries"][0]
+    return video["webpage_url"]
+
+# run on :8000
